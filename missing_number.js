52b6c8902dbfe664264c0f5b397b6c52ed00557d
Nathan Kawaller <kawaller@gmail.com>
2021-02-07 17:35:29 -0500

    missing numbers

---
+++ missing_number.js
@@ -0,0 +1,9 @@
+var missingNumber = function(nums) {
+    const length = nums.length;
+    let sum = ((length + 1) * length) / 2;
+
+    for (let i = 0; i < length; i++) {
+        sum = sum - nums[i];
+    }
+    return sum;
+}
