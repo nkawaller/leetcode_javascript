81a4474426218e78a403d3f43b29dc852cd36ccd
Nathan Kawaller <kawaller@gmail.com>
2021-02-07 21:31:56 -0500

    longest substring

---
+++ longest_substring_without_repeating_characters.js
@@ -0,0 +1,24 @@
+function lengthOfLongestSubstring(s) {
+    
+    if (s.length <= 1)
+        return s.length
+    
+    let lookup = new Map()
+    let len = s.length
+    let max = 0
+    let start = 0
+
+    for (let i = 0; i < len; i++) {
+        let c = s.charAt(i)
+
+        if (lookup.has(c) && lookup.get(c) >= start) {
+            start = lookup.get(c) + 1;
+        }
+
+        lookup.set(c, i)
+
+        max = Math.max(max, i - start + 1)
+    }
+
+    return max;
+}
