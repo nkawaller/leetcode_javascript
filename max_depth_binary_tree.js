ada61250cd57b33c7baa6d6976868c81f42947b3
Nathan Kawaller <kawaller@gmail.com>
2021-02-07 22:40:05 -0500

    max depth solution

---
+++ max_depth_binary_tree.js
@@ -0,0 +1,9 @@
+var maxDepth = function(root) {
+    let maxNode = (node, sum) => {
+        if (node === null) {
+            return sum;
+        }
+        return Math.max(maxNode(node.left, sum + 1), maxNode(node.right, sum +1));
+    }
+    return maxNode(root, 0);
+}
