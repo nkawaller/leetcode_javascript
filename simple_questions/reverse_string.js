5476ba795b5cdc55bf990f6682a2000d680f3494
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-02-12 11:36:28 -0500

    for of version

--- simple_questions/reverse_string.js
+++ simple_questions/reverse_string.js
@@ -8,6 +8,15 @@ function reverseString(s) {
 
 let revArrow = (s) => s.split('').reverse().join('')
 
+// For of loop
+function revForOf(s) {
+    let result = '';
+    for (i of s) {
+        result = i + result
+    }
+    return result
+}
+
 
 
 
@@ -15,3 +24,4 @@ a = 'Reversing a string in JavaScript'
 
 console.log(reverseString(a));
 console.log(revArrow(a));
+console.log(rev2(a));
