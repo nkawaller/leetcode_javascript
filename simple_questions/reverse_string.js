b0dbd6766dbfdb558da6298bdbfff1201467ba02
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-02-12 11:46:57 -0500

    decrementing for loop

--- simple_questions/reverse_string.js
+++ simple_questions/reverse_string.js
@@ -8,6 +8,16 @@ function reverseString(s) {
 let revArrow = (s) => s.split('').reverse().join('')
 
 
+// Decrementing for loop
+function decRev(s) {
+    let result = '';
+    for (let i = s.length - 1; i >= 0; i-- ) {
+        result += s[i];
+    }
+    return result;
+}
+
+
 // For of loop
 function revForOf(s) {
     let result = '';
@@ -28,12 +38,21 @@ function revForIn(s) {
 }
 
 
-
+function revWhile(s) {
+    let result = '';
+    let count = s[s.length];
+    while (count > 0) {
+        result += s[count];
+        count --;
+    }
+    return result
+}
 
 
 a = 'Reversing a string in JavaScript'
 
-console.log(reverseString(a));
-console.log(revArrow(a));
-console.log(revForOf(a));
-console.log(revForIn(a));
+// console.log(reverseString(a));
+// console.log(revArrow(a));
+// console.log(revForOf(a));
+// console.log(revForIn(a));
+console.log(decRev(a));
