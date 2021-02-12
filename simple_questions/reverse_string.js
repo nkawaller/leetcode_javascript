d24727c012c7d3add6a571281c33e8a25988cc6e
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-02-12 11:31:51 -0500

    arrow function version

--- simple_questions/reverse_string.js
+++ simple_questions/reverse_string.js
@@ -1,8 +1,17 @@
+// Traditional way
+
 function reverseString(s) {
     return s.split('').reverse().join('')
 }
+
+// Arrow function
+
+let revArrow = (s) => s.split('').reverse().join('')
+
+
 
 
 a = 'Reversing a string in JavaScript'
 
 console.log(reverseString(a));
+console.log(revArrow(a));
