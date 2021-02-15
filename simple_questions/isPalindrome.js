014b59d3177a0f77e6f12389053e9c9d9e0ac6cd
Nathan Kawaller <kawaller@gmail.com>
2021-02-15 09:35:41 -0500

    two palindrom solutions

---
+++ simple_questions/isPalindrome.js
@@ -0,0 +1,24 @@
+const isPalindrome = (s) => {
+	return s === s.split('').reverse().join('');
+}
+
+const isPal = (s) => {
+	let len = Math.floor(s.length / 2);
+	for (let i = 0; i < len; i++) {
+		if (s[i] !== s[s.length - i - 1]) {
+			return false;
+		} 
+	} return true;
+}
+
+
+
+
+
+let a = 'race a car';
+let b = 'racecar';
+
+console.log(isPalindrome(a));
+console.log(isPalindrome(b));
+console.log(isPal(a));
+console.log(isPal(b));
