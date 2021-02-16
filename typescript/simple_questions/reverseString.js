ed30cb8373167134da2a5fd2857d015fe198b28f
Nathan Kawaller <kawaller@gmail.com>
2021-02-16 13:42:08 -0500

    one liner

--- typescript/simple_questions/reverseString.js
+++ typescript/simple_questions/reverseString.js
@@ -1,5 +1,7 @@
 var reverseString = function (s) {
     return s.split('').reverse().join('');
 };
+var reverseOneLine = function (s) { return s.split('').reverse().join(''); };
 var a = 'Typescript reverse string';
 console.log(reverseString(a));
+console.log(reverseOneLine(a));
--- typescript/simple_questions/reverseString.ts
+++ typescript/simple_questions/reverseString.ts
@@ -2,5 +2,9 @@ const reverseString = (s: string): string => {
     return s.split('').reverse().join('')
 }
 
+const reverseOneLine = (s: string): string => s.split('').reverse().join('')
+
 let a = 'Typescript reverse string'
+
 console.log(reverseString(a))
+console.log(reverseOneLine(a))
