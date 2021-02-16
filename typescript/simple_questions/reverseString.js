d2e50cb385a07a6d72494dd10ecb5e4b412b1e6c
Nathan Kawaller <kawaller@gmail.com>
2021-02-16 15:35:11 -0500

    for of

--- typescript/simple_questions/reverseString.js
+++ typescript/simple_questions/reverseString.js
@@ -2,6 +2,16 @@ var reverseString = function (s) {
     return s.split('').reverse().join('');
 };
 var reverseOneLine = function (s) { return s.split('').reverse().join(''); };
+var reverseStringForOf = function (s) {
+    var result = '';
+    var charArray = s.split('');
+    for (var _i = 0, charArray_1 = charArray; _i < charArray_1.length; _i++) {
+        var i = charArray_1[_i];
+        result = i + result;
+    }
+    return result;
+};
 var a = 'Typescript reverse string';
 console.log(reverseString(a));
 console.log(reverseOneLine(a));
+console.log(reverseStringForOf(a));
--- typescript/simple_questions/reverseString.ts
+++ typescript/simple_questions/reverseString.ts
@@ -2,9 +2,21 @@ const reverseString = (s: string): string => {
     return s.split('').reverse().join('')
 }
 
+
 const reverseOneLine = (s: string): string => s.split('').reverse().join('')
+
+
+const reverseStringForOf = (s: string): string => {
+    let result: string = ''
+    let charArray: string[] = s.split('')
+    for (let i of charArray) {
+        result = i + result
+    }
+    return result
+}
 
 let a = 'Typescript reverse string'
 
 console.log(reverseString(a))
 console.log(reverseOneLine(a))
+console.log(reverseStringForOf(a))
