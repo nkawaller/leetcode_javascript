6e8014ff7cc836cebd57445c2578f69f4974b510
Nathan Kawaller <kawaller@gmail.com>
2021-02-23 07:50:01 -0500

    changed result type to any

--- typescript/simple_questions/chunkyMonkey.ts
+++ typescript/simple_questions/chunkyMonkey.ts
@@ -1,7 +1,7 @@
 // Split an array into groups of a specified size
 
 const chunkyMonkey = (arr: number[], size: number): number[] => {
-    const result: number[] = [];
+    const result: any[] = [];
     for (let i = 0; i < arr.length; i += size) {
         result.push(arr.slice(i, i + size));
     }
