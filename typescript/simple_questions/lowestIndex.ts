39a7995419e2df8e9e2cffc43e30ef6cea125625
Nathan Kawaller <kawaller@gmail.com>
2021-02-24 08:32:57 -0500

    formatted

--- typescript/simple_questions/lowestIndex.ts
+++ typescript/simple_questions/lowestIndex.ts
@@ -1,19 +1,17 @@
 // Return the lowest index at which a value should be inserted
 // into an array
 
-
 const lowestIndex = (arr: number[], val: number): number => {
-    arr.push(val)
-    arr.sort();
-    return arr.indexOf(val);
-}
-
+  arr.push(val);
+  arr.sort();
+  return arr.indexOf(val);
+};
 
 /**
  * Tests
  */
 
- let arrayWithoutVal: number[] = [1,2,3,5,6,7,8,9];
- let insertedValue: number = 4;
+let arrayWithoutVal: number[] = [1, 2, 3, 5, 6, 7, 8, 9];
+let insertedValue: number = 4;
 
- console.log(lowestIndex(arrayWithoutVal, insertedValue));
+console.log(lowestIndex(arrayWithoutVal, insertedValue));
