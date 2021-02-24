325f67bf07f0ccde7e3b3ce6a2fee0d1cbbac791
Nathan Kawaller <kawaller@gmail.com>
2021-02-24 08:44:37 -0500

    formatted and compiled

--- typescript/simple_questions/sliceAndSplice.ts
+++ typescript/simple_questions/sliceAndSplice.ts
@@ -1,14 +1,20 @@
 // Insert one array into another at a given index
 
+const sliceAndSplice = (
+  arr1: number[],
+  arr2: number[],
+  val: number
+): number[] => {
+  let newArr: number[] = [...arr1];
+  newArr.splice(val, 0, ...arr2);
+  return newArr;
+};
 
-const sliceAndSplice = (arr1: number[], arr2: number[], val: number): number[] => {
-    let newArr: number[] = [...arr1];
-    newArr.splice(val, 0, ...arr2);
-    return newArr;
-}
+/**
+ * Tests
+ */
 
-
-let firstArray: number[] = [1,5];
-let secondArray: number[] = [2,3,4]
+let firstArray: number[] = [1, 5];
+let secondArray: number[] = [2, 3, 4];
 
 console.log(sliceAndSplice(firstArray, secondArray, 1));
