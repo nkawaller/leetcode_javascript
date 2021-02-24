64eb1f033697db04ddec54066bd8c6a9cf449e03
Nathan Kawaller <kawaller@gmail.com>
2021-02-24 08:31:15 -0500

    compiled to js

---
+++ typescript/js_output/lowestIndex.js
@@ -0,0 +1,10 @@
+"use strict";
+const lowestIndex = (arr, val) => {
+    arr.push(val);
+    arr.sort();
+    return arr.indexOf(val);
+};
+let arrayWithoutVal = [1, 2, 3, 5, 6, 7, 8, 9];
+let insertedValue = 4;
+console.log(lowestIndex(arrayWithoutVal, insertedValue));
+//# sourceMappingURL=lowestIndex.js.map
