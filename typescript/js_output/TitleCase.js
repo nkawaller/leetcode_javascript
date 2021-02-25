56a92ab7fec6fec9852cf52e95f8a302fa28b6a2
Nathan Kawaller <kawaller@gmail.com>
2021-02-25 07:45:20 -0500

    compiled to js

---
+++ typescript/js_output/TitleCase.js
@@ -0,0 +1,9 @@
+"use strict";
+const TitleCase = (s) => {
+    return s.toLowerCase().split(' ').map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+};
+let lowerSentence = 'title case a sentence in javascript';
+console.log(TitleCase(lowerSentence));
+//# sourceMappingURL=TitleCase.js.map
