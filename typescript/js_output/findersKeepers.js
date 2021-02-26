98a6cb33ce26b5ee349693ccbda378254c6301dc
Nathan Kawaller <kawaller@gmail.com>
2021-02-26 11:59:35 -0500

    added string test

--- typescript/js_output/findersKeepers.js
+++ typescript/js_output/findersKeepers.js
@@ -3,6 +3,7 @@ let numArr = [1, 2, 3, 4];
 const findElement = (arr, func) => {
     return arr.find(func);
 };
-console.log(findElement(numArr, num => num % 2 == 0));
-let stringArr = ['cat', 'dog', 'pig', 'bird', 'walrus'];
+console.log(findElement(numArr, (num) => num % 2 == 0));
+let stringArr = ["cat", "dog", "pig", "bird", "walrus"];
+console.log(findElement(stringArr, (string) => string.length > 4));
 //# sourceMappingURL=findersKeepers.js.map
