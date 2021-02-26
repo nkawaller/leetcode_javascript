c8b922dd027e687fec23653efb3a42466e93f8c7
Nathan Kawaller <kawaller@gmail.com>
2021-02-26 11:57:46 -0500

    finders keepers init

---
+++ typescript/js_output/findersKeepers.js
@@ -0,0 +1,8 @@
+"use strict";
+let numArr = [1, 2, 3, 4];
+const findElement = (arr, func) => {
+    return arr.find(func);
+};
+console.log(findElement(numArr, num => num % 2 == 0));
+let stringArr = ['cat', 'dog', 'pig', 'bird', 'walrus'];
+//# sourceMappingURL=findersKeepers.js.map
---
+++ typescript/simple_questions/findersKeepers.ts
@@ -0,0 +1,19 @@
+// Find the first element in an array that meets a criteria
+
+
+let numArr: number[] = [1,2,3,4]
+
+const findElement = (arr: number[], func: any): any => {
+    return arr.find(func);
+}
+
+
+console.log(findElement(numArr, num => num % 2 == 0))
+
+
+
+
+
+
+let stringArr: string[] = ['cat', 'dog', 'pig', 'bird', 'walrus']
+
