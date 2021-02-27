7ae42e794df81d40d6be75cfaaf5b09cb4b648df
Nathan Kawaller <kawaller@gmail.com>
2021-02-27 09:18:15 -0500

    confirm ending one line

---
+++ typescript/js_output/confirmEnding.js
@@ -0,0 +1,9 @@
+"use strict";
+const confirmEndingOneLine = (str1, str2) => {
+    return str1.endsWith(str2);
+};
+let confirmStr1 = 'Learning Typescript';
+let confirmStr2 = 'script';
+let confirmStr3 = 'Learning';
+console.log(confirmEndingOneLine(confirmStr1, confirmStr2));
+//# sourceMappingURL=confirmEnding.js.map
---
+++ typescript/simple_questions/confirmEnding.ts
@@ -0,0 +1,15 @@
+// Return true if two strings have the same ending
+
+const confirmEndingOneLine = (str1: string, str2: string): boolean => {
+  return str1.endsWith(str2);
+};
+
+/**
+ * Tests
+ */
+
+let confirmStr1: string = "Learning Typescript";
+let confirmStr2: string = "script";
+let confirmStr3: string = "Learning";
+
+console.log(confirmEndingOneLine(confirmStr1, confirmStr2));
