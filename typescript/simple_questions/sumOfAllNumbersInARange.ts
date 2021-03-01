dff6b899739125a77aeb3d4e014e4a5a754bbf2a
Nathan Kawaller <kawaller@gmail.com>
2021-03-01 07:57:37 -0500

    initial solution

--- typescript/simple_questions/sumOfAllNumbersInARange.ts
+++ typescript/simple_questions/sumOfAllNumbersInARange.ts
@@ -2,3 +2,15 @@
 // numbers, plus all the numbers in between. The smallest 
 // number won't always come first.
 
+// sort the numbers... set?
+// for loop from a - b summing them
+
+const sumOfRange = (arr: number[]): number => {
+    let result: number = 0
+    arr = arr.sort()
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        result += i
+    }
+    return result
+
+}
