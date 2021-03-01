6c2b2bcc8dab6026677d9de46179d812608b2627
Nathan Kawaller <kawaller@gmail.com>
2021-03-01 09:10:40 -0500

    got the sort function to work

--- typescript/js_output/sumOfAllNumbersInARange.js
+++ typescript/js_output/sumOfAllNumbersInARange.js
@@ -1,14 +1,31 @@
 "use strict";
 const sumOfRange = (arr) => {
     let result = 0;
-    arr = arr.sort();
+    arr = arr.sort(function (a, b) {
+        return a - b;
+    });
     for (let i = arr[0]; i <= arr[1]; i++) {
+        result += i;
+    }
+    return result;
+};
+const sumOfRangeMinMax = (arr) => {
+    let result = 0;
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    for (let i = min; i <= max; i++) {
         result += i;
     }
     return result;
 };
 let arrOfTwoNumbers = [1, 3];
 let unsortedArrofTwoNumbers = [3, 1];
+let fiveAndTen = [5, 10];
+let unsortedFiveAndTen = [10, 5];
 console.log(sumOfRange(arrOfTwoNumbers));
 console.log(sumOfRange(unsortedArrofTwoNumbers));
+console.log(sumOfRange(fiveAndTen));
+console.log(sumOfRange(unsortedFiveAndTen));
+console.log(sumOfRangeMinMax(fiveAndTen));
+console.log(sumOfRangeMinMax(unsortedFiveAndTen));
 //# sourceMappingURL=sumOfAllNumbersInARange.js.map
--- typescript/simple_questions/sumOfAllNumbersInARange.ts
+++ typescript/simple_questions/sumOfAllNumbersInARange.ts
@@ -1,23 +1,39 @@
-// Given an array of two numbers, find the sum of those two 
-// numbers, plus all the numbers in between. The smallest 
+// Given an array of two numbers, find the sum of those two
+// numbers, plus all the numbers in between. The smallest
 // number won't always come first.
 
-
 const sumOfRange = (arr: number[]): number => {
-    let result: number = 0
-    arr = arr.sort()
-    for (let i = arr[0]; i <= arr[1]; i++) {
-        result += i
-    }
-    return result
+  let result: number = 0;
+  arr = arr.sort(function (a, b) {
+    return a - b;
+  });
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    result += i;
+  }
+  return result;
+};
 
-}
+const sumOfRangeMinMax = (arr: number[]): number => {
+  let result = 0;
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
+};
 
 /**
  * Tests
  */
 
-let arrOfTwoNumbers = [1,3]
-let unsortedArrofTwoNumbers = [3,1]
+let arrOfTwoNumbers = [1, 3];
+let unsortedArrofTwoNumbers = [3, 1];
+let fiveAndTen = [5, 10];
+let unsortedFiveAndTen = [10, 5];
 console.log(sumOfRange(arrOfTwoNumbers));
 console.log(sumOfRange(unsortedArrofTwoNumbers));
+console.log(sumOfRange(fiveAndTen));
+console.log(sumOfRange(unsortedFiveAndTen));
+console.log(sumOfRangeMinMax(fiveAndTen));
+console.log(sumOfRangeMinMax(unsortedFiveAndTen));
