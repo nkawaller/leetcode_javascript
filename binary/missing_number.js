63298f880ed8bf55f9f5be46d3b9c74b84096603
Nathan Kawaller <kawaller@gmail.com>
2021-03-02 08:59:26 -0500

    formatting

--- binary/missing_number.js
+++ binary/missing_number.js
@@ -1,9 +1,9 @@
-var missingNumber = function(nums) {
-    const length = nums.length;
-    let sum = ((length + 1) * length) / 2;
+var missingNumber = function (nums) {
+  const length = nums.length;
+  let sum = ((length + 1) * length) / 2;
 
-    for (let i = 0; i < length; i++) {
-        sum = sum - nums[i];
-    }
-    return sum;
-}
+  for (let i = 0; i < length; i++) {
+    sum = sum - nums[i];
+  }
+  return sum;
+};
--- binary/number_of_one_bits.js
+++ binary/number_of_one_bits.js
@@ -1,10 +1,10 @@
-var hammingWeight = function(n) {
-    var count = 0;
-    var arr = n.toString(2).split("");
-    for (var i in arr) {
-        if(arr[i] % 2 == 1) {
-            count ++;
-        }
+var hammingWeight = function (n) {
+  var count = 0;
+  var arr = n.toString(2).split("");
+  for (var i in arr) {
+    if (arr[i] % 2 == 1) {
+      count++;
     }
-    return count;
+  }
+  return count;
 };
