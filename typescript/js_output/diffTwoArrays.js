fad6148791e48bbc4a154ec780c4c255807db23f
Nathan Kawaller <kawaller@gmail.com>
2021-03-03 07:43:25 -0500

    testing arrays of strings as well

--- typescript/js_output/diffTwoArrays.js
+++ typescript/js_output/diffTwoArrays.js
@@ -1,11 +1,13 @@
 "use strict";
 const diffTwoArrays = (x, y) => {
-    const a = x.filter((item) => !y.includes(item));
-    const b = y.filter((item) => !x.includes(item));
-    const difference = a.concat(b);
-    return difference;
+    const filteredArr1 = x.filter((item) => !y.includes(item));
+    const filteredArr2 = y.filter((item) => !x.includes(item));
+    return filteredArr1.concat(filteredArr2);
 };
 let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [4, 5, 6];
+let stringArr1 = ['cat', 'dog', 'bird', 'fish'];
+let stringArr2 = ['bird', 'fish', 'pig', 'horse'];
 console.log(diffTwoArrays(arr1, arr2));
+console.log(diffTwoArrays(stringArr1, stringArr2));
 //# sourceMappingURL=diffTwoArrays.js.map
--- typescript/simple_questions/diffTwoArrays.ts
+++ typescript/simple_questions/diffTwoArrays.ts
@@ -1,14 +1,16 @@
 // Compare two arrays and return a new array with any items found
 // in only one of the arrays (symmetric difference)
 
-const diffTwoArrays = (x: number[], y: number[]): number[] => {
-  const a: number[] = x.filter((item) => !y.includes(item));
-  const b: number[] = y.filter((item) => !x.includes(item));
-  const difference: number[] = a.concat(b);
-  return difference;
+const diffTwoArrays = (x: any[], y: any[]): any[] => {
+  const filteredArr1: any[] = x.filter((item) => !y.includes(item));
+  const filteredArr2: any[] = y.filter((item) => !x.includes(item));
+  return filteredArr1.concat(filteredArr2);
 };
 
 let arr1: number[] = [1, 2, 3, 4, 5];
 let arr2: number[] = [4, 5, 6];
+let stringArr1: string[] = ['cat', 'dog', 'bird', 'fish']
+let stringArr2: string[] = ['bird', 'fish', 'pig', 'horse']
 
 console.log(diffTwoArrays(arr1, arr2));
+console.log(diffTwoArrays(stringArr1, stringArr2));
