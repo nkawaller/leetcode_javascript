c0eb0e16600eafacd490a2b0e3d4f3fb3e6f28c8
Nathan Kawaller <kawaller@gmail.com>
2021-03-05 09:03:40 -0500

    all tests pass

--- simple_questions/factorial.test.js
+++ simple_questions/factorial.test.js
@@ -8,13 +8,13 @@ test("find the factorial of a number", () => {
   expect(fact(7)).toBe(5040);
 });
 
-// test("find factorial using for...Of", () => {
-//   expect(factForOf(5)).toBe(120);
-// });
+test("find factorial using for...Of", () => {
+  expect(factForOf([1, 2, 3, 4, 5])).toBe(120);
+});
 
-// test("find factorial using for...Of", () => {
-//   expect(factForOf(7)).toEqual(5040);
-// });
+test("find factorial using for...Of", () => {
+  expect(factForOf([1, 2, 3, 4, 5, 6, 7])).toEqual(5040);
+});
 
 test("find factorial recursively", () => {
   expect(recursiveFact(5)).toEqual(120);
@@ -24,10 +24,10 @@ test("find factorial recursively", () => {
   expect(recursiveFact(7)).toEqual(5040);
 });
 
-// test("find factorial using reduce", () => {
-//   expect(factReduce(5)).toEqual(120);
-// });
+test("find factorial using reduce", () => {
+  expect(factReduce([1, 2, 3, 4, 5])).toEqual(120);
+});
 
-// test("find factorial using reduce", () => {
-//   expect(factReduce(7)).toBe(5040);
-// });
+test("find factorial using reduce", () => {
+  expect(factReduce([1, 2, 3, 4, 5, 6, 7])).toBe(5040);
+});
