2224efaf23a4b597e07205804bd6a7276d9bc2cd
Nathan Kawaller <kawaller@gmail.com>
2021-03-08 08:45:08 -0500

    finished tests

--- simple_questions/remove_false_values.test.js
+++ simple_questions/remove_false_values.test.js
@@ -5,9 +5,20 @@ const {
   removeFalseForIn,
 } = require("./remove_false_values");
 
-
 let booleanArray = [true, true, false, true, false];
 
-test('Remove false values from an array', () => {
-    expect(removeFalseOneLine(booleanArray)).toStrictEqual([true, true, true])
-})
+test("Remove false values from an array", () => {
+  expect(removeFalseOneLine(booleanArray)).toStrictEqual([true, true, true]);
+});
+
+test("Remove false values from an array", () => {
+  expect(removeFalse(booleanArray)).toStrictEqual([true, true, true]);
+});
+
+test("Remove false values from an array", () => {
+  expect(removeFalseForOf(booleanArray)).toStrictEqual([true, true, true]);
+});
+
+test("Remove false values from an array", () => {
+  expect(removeFalseForIn(booleanArray)).toStrictEqual([true, true, true]);
+});
