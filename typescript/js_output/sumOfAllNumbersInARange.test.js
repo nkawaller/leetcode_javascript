37af0c544acc77fb4c6cfc14c8aecd784ec34258
Nathan Kawaller <kawaller@gmail.com>
2021-03-11 07:24:30 -0500

    compiled to js

--- typescript/js_output/sumOfAllNumbersInARange.test.js
+++ typescript/js_output/sumOfAllNumbersInARange.test.js
@@ -4,17 +4,53 @@ let arrOfTwoNumbers = [1, 3];
 let unsortedArrofTwoNumbers = [3, 1];
 let fiveAndTen = [5, 10];
 let unsortedFiveAndTen = [10, 5];
-test('Sum up all the numbers in a range', () => {
+test("Sum up all the numbers in a range", () => {
     expect(sumOfRange(arrOfTwoNumbers)).toEqual(6);
 });
-test('Sum up all the numbers in a range', () => {
+test("Sum up all the numbers in a range", () => {
     expect(sumOfRange(unsortedArrofTwoNumbers)).toEqual(6);
 });
-test('Sum up all the numbers in a range', () => {
+test("Sum up all the numbers in a range", () => {
     expect(sumOfRange(fiveAndTen)).toEqual(45);
 });
-test('Sum up all the numbers in a range', () => {
+test("Sum up all the numbers in a range", () => {
     expect(sumOfRange(unsortedFiveAndTen)).toEqual(45);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeMinMax(arrOfTwoNumbers)).toEqual(6);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeMinMax(unsortedArrofTwoNumbers)).toEqual(6);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeMinMax(fiveAndTen)).toEqual(45);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeMinMax(unsortedFiveAndTen)).toEqual(45);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeRecursive(arrOfTwoNumbers)).toEqual(6);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeRecursive(unsortedArrofTwoNumbers)).toEqual(6);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeRecursive(fiveAndTen)).toEqual(45);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeRecursive(unsortedFiveAndTen)).toEqual(45);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeReduce(arrOfTwoNumbers)).toEqual(6);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeReduce(unsortedArrofTwoNumbers)).toEqual(6);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeReduce(fiveAndTen)).toEqual(45);
+});
+test("Sum up all the numbers in a range", () => {
+    expect(sumOfRangeReduce(unsortedFiveAndTen)).toEqual(45);
 });
 module.exports = {};
 //# sourceMappingURL=sumOfAllNumbersInARange.test.js.map
