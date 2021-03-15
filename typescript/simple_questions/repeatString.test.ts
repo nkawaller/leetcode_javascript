84d407c4d72ca72a30e6f25249436844947ec1a2
Nathan Kawaller <kawaller@gmail.com>
2021-03-15 09:29:45 -0400

    finished tests

--- typescript/simple_questions/repeatString.test.ts
+++ typescript/simple_questions/repeatString.test.ts
@@ -1,11 +1,23 @@
-export = {}
+export = {};
 const {
-    repeatString,
-    repeatStringOneLine,
-    repeatStringWhile,
-    repeatStringRecursive
-} = require('./repeatString')
+  repeatString,
+  repeatStringOneLine,
+  repeatStringWhile,
+  repeatStringRecursive,
+} = require("./repeatString");
 
-test('Repeat a string a given amount of times', () => {
-    expect(repeatStringOneLine('test', 3)).toBe('testtesttest');
-})
+test("Repeat a string a given amount of times", () => {
+  expect(repeatStringOneLine("test", 3)).toBe("testtesttest");
+});
+
+test("Repeat a string a given amount of times", () => {
+  expect(repeatString("test", 3)).toBe("testtesttest");
+});
+
+test("Repeat a string a given amount of times", () => {
+  expect(repeatStringWhile("test", 3)).toBe("testtesttest");
+});
+
+test("Repeat a string a given amount of times", () => {
+  expect(repeatStringRecursive("test", 3)).toBe("testtesttest");
+});
