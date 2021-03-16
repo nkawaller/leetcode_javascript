ab3b2153f9da6f110144777b952f5ed213091c7d
Nathan Kawaller <kawaller@gmail.com>
2021-03-16 07:42:29 -0400

    finished tests

--- typescript/js_output/remove_false_values.test.js
+++ typescript/js_output/remove_false_values.test.js
@@ -4,5 +4,14 @@ let arrOfBools = [true, true, false, true, false];
 test("Remove all false values from an array", () => {
     expect(removeFalseOne(arrOfBools)).toStrictEqual([true, true, true]);
 });
+test("Remove all false values from an array", () => {
+    expect(removeFalse(arrOfBools)).toStrictEqual([true, true, true]);
+});
+test("Remove all false values from an array", () => {
+    expect(removeFalseForIn(arrOfBools)).toStrictEqual([true, true, true]);
+});
+test("Remove all false values from an array", () => {
+    expect(removeFalseForOf(arrOfBools)).toStrictEqual([true, true, true]);
+});
 module.exports = {};
 //# sourceMappingURL=remove_false_values.test.js.map
--- typescript/simple_questions/remove_false_values.test.ts
+++ typescript/simple_questions/remove_false_values.test.ts
@@ -12,3 +12,15 @@ let arrOfBools: boolean[] = [true, true, false, true, false];
 test("Remove all false values from an array", () => {
   expect(removeFalseOne(arrOfBools)).toStrictEqual([true, true, true]);
 });
+
+test("Remove all false values from an array", () => {
+  expect(removeFalse(arrOfBools)).toStrictEqual([true, true, true]);
+});
+
+test("Remove all false values from an array", () => {
+  expect(removeFalseForIn(arrOfBools)).toStrictEqual([true, true, true]);
+});
+
+test("Remove all false values from an array", () => {
+  expect(removeFalseForOf(arrOfBools)).toStrictEqual([true, true, true]);
+});
