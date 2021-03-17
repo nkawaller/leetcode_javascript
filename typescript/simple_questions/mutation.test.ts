aa65f8489155c961612e089bb40baf91918775e7
Nathan Kawaller <kawaller@gmail.com>
2021-03-17 08:35:19 -0400

    wrote tests

--- typescript/simple_questions/mutation.test.ts
+++ typescript/simple_questions/mutation.test.ts
@@ -1,5 +1,13 @@
-export = {}
-const mutation = require('./mutation')
+export = {};
+const mutation = require("./mutation");
 
-let testArrFalse = ['plan', 'planet']
-let testArrTrue = ['planet', 'plan']
+let testArrFalse = ["plan", "planet"];
+let testArrTrue = ["planet", "plan"];
+
+test("Does the first element in the array contain all the letters in the second element", () => {
+  expect(mutation(testArrFalse)).toBeFalsy();
+});
+
+test("Does the first element in the array contain all the letters in the second element", () => {
+  expect(mutation(testArrTrue)).toBeTruthy();
+});
