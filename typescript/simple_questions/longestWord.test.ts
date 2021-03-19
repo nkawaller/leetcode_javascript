083c4ab4122d6dc84049beecc9da8010c8b0470d
Nathan Kawaller <kawaller@gmail.com>
2021-03-19 06:31:58 -0400

    wrote test

--- typescript/simple_questions/longestWord.test.ts
+++ typescript/simple_questions/longestWord.test.ts
@@ -2,3 +2,9 @@ export = {}
 
 const findLongestWord = require('./longestWord')
 
+
+let stringOfWords: string = "Trying to find the longest word in this string";
+
+test('Find the length of the longest word in a string', () => {
+    expect(findLongestWord(stringOfWords)).toBe(7)
+})
