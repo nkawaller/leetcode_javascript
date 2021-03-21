fc65a6ef935b758bf9406b9a8ab7aefbd246a8cc
Nathan Kawaller <kawaller@gmail.com>
2021-03-20 22:07:20 -0400

    wrote test

--- typescript/simple_questions/isPalindrome.test.ts
+++ typescript/simple_questions/isPalindrome.test.ts
@@ -1,7 +1,30 @@
 export = {};
 
-const {
-    isPalForEach,
-    isPalEvery,
-    isPalReduce
-} = require('./isPalindrome')
+const { isPalForEach, isPalEvery, isPalReduce } = require("./isPalindrome");
+
+let aPal: string = "A man, a plan, a canal: Panama";
+let bPal: string = "Race a car";
+
+test("Determine whether a string is a palindrome", () => {
+  expect(isPalForEach(aPal)).toBeTruthy;
+});
+
+test("Determine whether a string is a palindrome", () => {
+  expect(isPalForEach(bPal)).toBeFalsy;
+});
+
+test("Determine whether a string is a palindrome", () => {
+  expect(isPalEvery(aPal)).toBeTruthy;
+});
+
+test("Determine whether a string is a palindrome", () => {
+  expect(isPalEvery(bPal)).toBeFalsy;
+});
+
+test("Determine whether a string is a palindrome", () => {
+  expect(isPalReduce(aPal)).toBeTruthy;
+});
+
+test("Determine whether a string is a palindrome", () => {
+  expect(isPalReduce(bPal)).toBeFalsy;
+});
