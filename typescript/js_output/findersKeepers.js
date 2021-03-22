cdc38bf9d37228f4aaff4044bbb856b7f69beeb6
Nathan Kawaller <kawaller@gmail.com>
2021-03-21 20:21:31 -0400

    cleaned up and compiled

--- typescript/js_output/findersKeepers.js
+++ typescript/js_output/findersKeepers.js
@@ -3,11 +3,9 @@ let numArr = [1, 2, 3, 4];
 const findElement = (arr, num) => {
     return arr.find(num);
 };
-console.log(findElement(numArr, (num) => num > 3));
 let stringArr = ["cat", "dog", "pig", "bird", "walrus"];
 const findStringElement = (arr, str) => {
     return arr.find(str);
 };
-console.log(findStringElement(stringArr, (string) => string.length > 4));
 module.exports = { findElement, findStringElement };
 //# sourceMappingURL=findersKeepers.js.map
--- typescript/simple_questions/findersKeepers.ts
+++ typescript/simple_questions/findersKeepers.ts
@@ -3,27 +3,21 @@
 let numArr: number[] = [1, 2, 3, 4];
 
 interface numFunc {
-    (num: number): boolean
+  (num: number): boolean;
 }
 
 const findElement = (arr: number[], num: numFunc): any => {
   return arr.find(num);
 };
 
-console.log(findElement(numArr, (num) => num > 3));
-
-
 interface stringFunc {
-    (str: string): boolean
+  (str: string): boolean;
 }
 
 let stringArr: string[] = ["cat", "dog", "pig", "bird", "walrus"];
 
 const findStringElement = (arr: string[], str: stringFunc): any => {
-    return arr.find(str)
-}
-
-console.log(findStringElement(stringArr, (string) => string.length > 4));
-
+  return arr.find(str);
+};
 
 module.exports = { findElement, findStringElement };
