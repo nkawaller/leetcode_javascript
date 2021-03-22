c08377b4437480ddb001c143f563cba0a33a43f0
Nathan Kawaller <kawaller@gmail.com>
2021-03-21 20:20:46 -0400

    second test

--- typescript/simple_questions/findersKeepers.test.ts
+++ typescript/simple_questions/findersKeepers.test.ts
@@ -9,3 +9,9 @@ let stringArr: string[] = ["cat", "dog", "pig", "bird", "walrus"];
 test("Find the first number in an array to meet a certian criteria", () => {
   expect(findElement(numArr, (num: number) => num > 3)).toBe(4);
 });
+
+test("Find the first string in an array to meet a certian criteria", () => {
+  expect(findElement(stringArr, (string: string) => string.length > 4)).toBe(
+    "walrus"
+  );
+});
