8a772c1c201b21a5dce1219b47061eecbc3efd1d
Nathan Kawaller <kawaller@gmail.com>
2021-03-22 16:57:21 -0400

    reduce tests

--- typescript/simple_questions/factorial.test.ts
+++ typescript/simple_questions/factorial.test.ts
@@ -5,3 +5,15 @@ const { fact, factForOf, factReduce, factRecursive } = require('./factorial')
 test('Find the factorial of a given number', () => {
     expect(fact(5)).toBe(120);
 })
+
+test('Find the factorial of a given number', () => {
+    expect(fact(7)).toBe(5040);
+})
+
+test('Find the factorial of a given number', () => {
+    expect(factRecursive(5)).toBe(120);
+})
+
+test('Find the factorial of a given number', () => {
+    expect(factRecursive(7)).toBe(5040);
+})
