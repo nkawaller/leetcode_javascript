2421d52fc6beac150ee93d5326fb67271714870d
Nathan Kawaller <kawaller@gmail.com>
2021-03-22 16:59:58 -0400

    reduce and for of tests

--- typescript/simple_questions/factorial.test.ts
+++ typescript/simple_questions/factorial.test.ts
@@ -17,3 +17,19 @@ test('Find the factorial of a given number', () => {
 test('Find the factorial of a given number', () => {
     expect(factRecursive(7)).toBe(5040);
 })
+
+test('Find the factorial of a given number', () => {
+    expect(factReduce([1,2,3,4,5])).toBe(120);
+})
+
+test('Find the factorial of a given number', () => {
+    expect(factReduce([1,2,3,4,5,6,7])).toBe(5040);
+})
+
+test('Find the factorial of a given number', () => {
+    expect(factForOf([1,2,3,4,5])).toBe(120);
+})
+
+test('Find the factorial of a given number', () => {
+    expect(factForOf([1,2,3,4,5,6,7])).toBe(5040);
+})
