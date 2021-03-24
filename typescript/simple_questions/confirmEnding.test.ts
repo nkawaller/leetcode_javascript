1ea08f833c61a030aaa2fe10938f2ea073f07d9e
Nathan Kawaller <kawaller@gmail.com>
2021-03-24 18:28:24 -0400

    wrote first tests

--- typescript/simple_questions/confirmEnding.test.ts
+++ typescript/simple_questions/confirmEnding.test.ts
@@ -1,4 +1,15 @@
-export = {}
+export = {};
 
-const { confirmEndingOneLine, confirmEndingSlice } = require('./confirmEnding');
+const { confirmEndingOneLine, confirmEndingSlice } = require("./confirmEnding");
 
+let confirmStr1: string = "Learning Typescript";
+let confirmStr2: string = "script";
+let confirmStr3: string = "Learning";
+
+test("Determine whether two string have the same ending", () => {
+  expect(confirmEndingOneLine(confirmStr1, confirmStr2)).toBeTruthy;
+});
+
+test("Determine whether two string have the same ending", () => {
+  expect(confirmEndingOneLine(confirmStr1, confirmStr3)).toBeTruthy;
+});
