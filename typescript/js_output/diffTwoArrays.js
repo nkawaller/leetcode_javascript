03be74b9a3307f1bfb8e65a2794521f80308a8bd
Nathan Kawaller <kawaller@gmail.com>
2021-03-23 20:05:21 -0400

    cleaned up and compiled

--- typescript/js_output/diffTwoArrays.js
+++ typescript/js_output/diffTwoArrays.js
@@ -4,11 +4,5 @@ const diffTwoArrays = (x, y) => {
     const filteredArr2 = y.filter((item) => !x.includes(item));
     return filteredArr1.concat(filteredArr2);
 };
-let arr1 = [1, 2, 3, 4, 5];
-let arr2 = [4, 5, 6];
-let stringArr1 = ["cat", "dog", "bird", "fish"];
-let stringArr2 = ["bird", "fish", "pig", "horse"];
-console.log(diffTwoArrays(arr1, arr2));
-console.log(diffTwoArrays(stringArr1, stringArr2));
 module.exports = diffTwoArrays;
 //# sourceMappingURL=diffTwoArrays.js.map
--- typescript/simple_questions/diffTwoArrays.ts
+++ typescript/simple_questions/diffTwoArrays.ts
@@ -7,16 +7,4 @@ const diffTwoArrays = (x: any[], y: any[]): any[] => {
   return filteredArr1.concat(filteredArr2);
 };
 
-/**
- * Tests
- */
-
-let arr1: number[] = [1, 2, 3, 4, 5];
-let arr2: number[] = [4, 5, 6];
-let stringArr1: string[] = ["cat", "dog", "bird", "fish"];
-let stringArr2: string[] = ["bird", "fish", "pig", "horse"];
-
-console.log(diffTwoArrays(arr1, arr2));
-console.log(diffTwoArrays(stringArr1, stringArr2));
-
 module.exports = diffTwoArrays;
