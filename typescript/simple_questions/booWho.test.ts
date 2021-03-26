e290f938632a1dcd7ae1e9a46289d4c1e44e73c5
Nathan Kawaller <kawaller@gmail.com>
2021-03-26 18:28:27 -0400

    first test

--- typescript/simple_questions/booWho.test.ts
+++ typescript/simple_questions/booWho.test.ts
@@ -1,2 +1,11 @@
 export = {}
-const booWho = require('booWho');
+const booWho = require('./booWho');
+
+
+let boolCheckTrue = true;
+let boolCheckFalse = "hello";
+
+test('Check whether a value is a boolean', () => {
+    expect(booWho(boolCheckTrue)).toBeTruthy
+})
+
