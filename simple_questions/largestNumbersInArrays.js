2ae99312feef2780535c33afcc974517296e4ae6
Nathan Kawaller <kawaller@gmail.com>
2021-04-11 09:40:38 -0400

    largest numbers in arrays test

--- simple_questions/largestNumbersInArrays.js
+++ simple_questions/largestNumbersInArrays.js
@@ -1,13 +1,11 @@
 // Return the largest number in each array
 
-let a = [[1,2,3], [1,2,3], [1,2,3]]
-
 const largestNumber = (arr) => {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(Math.max(...arr[i]));
-    }
-    return result
-}
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(Math.max(...arr[i]));
+  }
+  return result;
+};
 
-console.log(largestNumber(a));
+module.exports = largestNumber;
---
+++ simple_questions/largestNumbersInArrays.test.js
@@ -0,0 +1,7 @@
+const largestNumber = require('./largestNumbersInArrays');
+
+let a = [[1,2,3], [1,2,3], [1,2,3]]
+
+test('Return the largest number from each array', () => {
+    expect(largestNumber(a)).toStrictEqual([3,3,3])
+})
