bc3e0e1f3596d72356d4ce8f893bf7bf7988cf78
Nathan Kawaller <kawaller@gmail.com>
2021-04-12 00:34:55 -0400

    cleaning up

--- strings/longest_palindromic_substring.js
+++ strings/longest_palindromic_substring.js
@@ -1,26 +1,4 @@
-function longestPalindrome(s) {
-  var longest = "",
-    c;
-  for (c = 0; c < s.length; c++) {
-    if ((s.length - c) * 2 <= longest.length) break;
-
-    longest = scanOutward(s, longest, c, c);
-    longest = scanOutward(s, longest, c + 1, c);
-  }
-  return longest;
-}
-
-function scanOutward(s, longest, left, right) {
-  if (left > 0 && right < s.length && s[left - 1] === s[right + 1]) {
-    return scanOutward(s, longest, left - 1, right + 1);
-  } else {
-    return right - left + 1 >= longest.length
-      ? s.substring(left, right + 1)
-      : longest;
-  }
-}
-
-const longestPalindromicSubstring = (str) => {
+const longestPalindrome = (str) => {
   if (str === undefined || str === null || str.length < 1) {
     return "";
   }
